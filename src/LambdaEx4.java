ac638676559e46f2c82abd83ae31fa60093f3ed4
East_Start_김동규 <qweszx33@naver.com>
2023-03-14 10:32:23 +0900

    test & chore:create LambdaEx4 & add Intellij program create-time:2023-03-14 10:32

---
+++ src/LambdaEx4.java
@@ -0,0 +1,33 @@
+@FunctionalInterface
+interface MyFunction2{
+    void myMethod();
+}
+
+class Outer{
+    int val = 10; //Outer.this.val
+
+    class Inner{
+        int val = 20; //this.val
+
+        void method(int i){
+            int val = 30; //final int val = 30;
+
+            MyFunction2 f = () -> {
+                System.out.println("            i:" + i);
+                System.out.println("          val:" + val);
+                System.out.println("     this.val:" + ++this.val);
+                System.out.println("Outer.this.val:" + ++Outer.this.val);
+            };
+
+            f.myMethod();
+        }
+    }
+}
+
+class LambdaEx4{
+    public static void main(String args[]){
+        Outer outer = new Outer();
+        Outer.Inner inner = outer.new Inner();
+        inner.method(100);
+    }
+}
